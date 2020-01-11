11449491d668f4cb4e7247d5808df3d6a8179e9c
Samuel Coleman <smcoleman19@gmail.com>
2020-01-11 12:21:35 -0700

    Build basic SearchForm component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="search-form">
+    <form>
+      <label>Search Images</label>
+      <input type="text" />
+      <button>Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchForm',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
